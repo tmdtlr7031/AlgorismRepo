b944c1c8f344c3d858a7a739d02b1a5b40a874a7
namseungsik <tmd7031@naver.com>
2021-08-26 10:13:39 +0900

    주석 추가

--- python/Programmers/문자열 내 마음대로 정렬하기.py
+++ python/Programmers/문자열 내 마음대로 정렬하기.py
@@ -1,4 +1,5 @@
 # 문자열 내 마음대로 정렬하기 https://programmers.co.kr/learn/courses/30/lessons/12915
+# x[n], x => n번째 기준으로 정렬, 같은 경우 사전순으로 정렬
 def solution(strings, n):
     return sorted(strings, key=lambda x: (x[n], x))
 
